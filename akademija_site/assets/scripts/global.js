925df4399fa1c46cfebfbb56e577c10c02889501
dragantomas <32159088+dragantomas@users.noreply.github.com>
2018-03-24 22:46:56 +0100

    domas 2246 240318

--- akademija_site/assets/scripts/global.js
+++ akademija_site/assets/scripts/global.js
@@ -17,27 +17,13 @@ document.getElementById('btn2').addEventListener('click', showDropdown);
 document.getElementById('btn3').addEventListener('click',showDropdown);
 document.getElementById('btn4').addEventListener('click', showDropdown); 
 document.getElementById('btn5').addEventListener('click',showDropdown);
-document.getElementById('btn6').addEventListener('click', showDropdown);
+document.getElementById('btn6').addEventListener('click',showDropdown);
 
 function showDropdown(event){
   if (activeDropdown.id && activeDropdown.id !== event.target.id) {
     activeDropdown.element.classList.remove('active');
   }
 
-  //checking if a list element was clicked, changing the inner button value
-  if (event.target.tagName === 'LI') {
-    activeDropdown.button.innerHTML = event.target.innerHTML;
-    for (var i=0;i<event.target.parentNode.children.length;i++){
-      if (event.target.parentNode.children[i].classList.contains('check')) {
-        event.target.parentNode.children[i].classList.remove('check');
-      }
-    }
-    
-    //timeout here so the check is only visible after opening the dropdown again
-    window.setTimeout(function(){
-          event.target.classList.add('check');
-    },500)
-  }
   for (var i = 0;i<this.children.length;i++){
     if (this.children[i].classList.contains('dropdown-selection')){
         activeDropdown.id = this.id;
@@ -51,8 +37,32 @@ function showDropdown(event){
   }
 }
 
-window.onclick = function(event) {
-  if (!event.target.classList.contains('dropdown-button')) {
-    activeDropdown.element.classList.remove('active');
+var activeDropdownSearch = {};
+
+document.getElementById('btn6').addEventListener('click', showDropdownSearch);
+
+function showDropdownSearch(event){
+  if (activeDropdownSearch.id && activeDropdownSearch.id !== event.target.id) {
+    activeDropdownSearch.element.classList.remove('active');
+  }
+
+  for (var i = 0;i<this.children.length;i++){
+    if (this.children[i].classList.contains('dropdown-selection-search')){
+        activeDropdownSearch.id = this.id;
+        activeDropdownSearch.element = this.children[i];
+        this.children[i].classList.add('active');
+     }
+    //adding the dropdown-button to our object
+    else if (this.children[i].classList.contains('dropdown-selection-search')){
+      activeDropdownSearch.button = this.children[i];
+    }
   }
 }
+
+window.onclick = function(event) {
+  if (!event.target.classList.contains('dropdown-button') && !event.target.classList.contains('dropdown-button-search')) {
+    activeDropdown.element.classList.remove('active');
+    activeDropdownSearch.element.classList.remove('active');
+  }
+}
+
