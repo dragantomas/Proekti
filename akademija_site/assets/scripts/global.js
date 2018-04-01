492ab579c5c5ea1266c8cea4d5b6c385ebb59178
dragantomas <32159088+dragantomas@users.noreply.github.com>
2018-04-01 19:52:18 +0200

    doma 1952 010418

--- akademija_site/assets/scripts/global.js
+++ akademija_site/assets/scripts/global.js
@@ -11,6 +11,8 @@ function myFunction() {
   }
 }
 
+// DROP DOWN MENU
+
 var activeDropdown = {};
 document.getElementById('btn1').addEventListener('click',showDropdown);
 document.getElementById('btn2').addEventListener('click', showDropdown);
@@ -18,6 +20,11 @@ document.getElementById('btn3').addEventListener('click',showDropdown);
 document.getElementById('btn4').addEventListener('click', showDropdown); 
 document.getElementById('btn5').addEventListener('click',showDropdown);
 document.getElementById('btn6').addEventListener('click',showDropdown);
+document.getElementById('btn1b').addEventListener('click',showDropdown);
+document.getElementById('btn2b').addEventListener('click', showDropdown);
+document.getElementById('btn3b').addEventListener('click',showDropdown);
+document.getElementById('btn4b').addEventListener('click', showDropdown); 
+document.getElementById('btn5b').addEventListener('click',showDropdown);
 
 function showDropdown(event){
   if (activeDropdown.id && activeDropdown.id !== event.target.id) {
@@ -65,4 +72,22 @@ window.onclick = function(event) {
     activeDropdownSearch.element.classList.remove('active');
   }
 }
+
+// БУРГЕР МЕНУ
+
+var burgerMenu = document.querySelector('.burger-menu');
+var burgerMenuDrop = document.querySelector('.burger-menu-drop');
+
+burgerMenu.addEventListener('click', showBurgerMenuDrop);
+
+
+function showBurgerMenuDrop() {
+  if (burgerMenuDrop.classList.contains("burger-menu-click")) {
+    burgerMenuDrop.classList.remove("burger-menu-click");
+    burgerMenuDrop.classList.add("burger-menu-drop")
+  }  else {
+    burgerMenuDrop.classList.remove("burger-menu-drop");
+    burgerMenuDrop.classList.add("burger-menu-click");
+  }
+};
 
