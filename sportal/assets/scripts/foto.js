66cc5594cfad1a395314c55ae6eb49c0260388ac
Dragan Tomovski <32159088+dragantomas@users.noreply.github.com>
2018-05-23 11:39:36 +0200

    semos 1139 230518

--- sportal/assets/scripts/foto.js
+++ sportal/assets/scripts/foto.js
@@ -98,11 +98,9 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    // for (i = 0; i < dots.length; i++) {
-    //     dots[i].className = dots[i].className.replace(" active", "");
-    // }
+
     slides[slideIndex - 1].style.display = "block";
-    // dots[slideIndex - 1].className += " active";
+
 }
 
 function openModal() {
@@ -135,7 +133,5 @@ function showSlidesLb(n) {
     }
 
     slidesLb[slideIndexLb - 1].style.display = "block";
-    // dots[slideIndexLb - 1].className += " active";
-    // captionText.innerHTML = dots[slideIndexLb - 1].alt;
 }
 
